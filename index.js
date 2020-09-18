28800a1d6e768010193f2032c2b8f611f9df78ac
David Felix <david.felix02@hotmail.com>
2020-09-18 01:22:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ let multiply = num1*num2;
 
 let random = Math.floor(Math.random()*Infinity)+1;
 
-let mod = 0%4;
+let mod = 79%5;
+
+let max = Math.max(1,2,3,4,5,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
